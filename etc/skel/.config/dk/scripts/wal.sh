d49cc1b6d2447460f26ad3fc02f6276885eb985a
arcolinuxz <arcolinuxinfo@gmail.com>
2023-10-14 08:33:01 +0200

    backgrounds

--- etc/skel/.config/dk/scripts/wal.sh
+++ etc/skel/.config/dk/scripts/wal.sh
@@ -4,11 +4,11 @@
 ### Check if python-pywal is installed
 ### Check if dir exists and copy the template in its proper place
 if [ -x "$(command -v wal)" ]; then
-    my_array=($HOME/.config/i3/backgrounds/*)
+    my_array=($HOME/.config/dk/backgrounds/*)
     wal -i ${my_array[$(( $RANDOM % ${#my_array[@]}))]} -a 80
 else
   # Set background
   if [ -x "$(command -v feh)" ]; then
-    feh --randomize --bg-fill $HOME/.config/i3/backgrounds/*
+    feh --randomize --bg-fill $HOME/.config/dk/backgrounds/*
   fi
 fi
