80bcffb5d29ae8b723fe52e5f84cae00c73fefdc
Erik Dubois <erik.dubois@gmail.com>
2025-03-31 07:47:41 +0200

    arco wallpaper

--- etc/skel/.config/dk/autostart.sh
+++ etc/skel/.config/dk/autostart.sh
@@ -22,7 +22,7 @@ run sxhkd -c ~/.config/dk/sxhkd/sxhkdrc &
 #Some ways to set your wallpaper besides variety or nitrogen
 #feh --bg-scale ~/.config/dk/wall.png &
 feh --bg-fill /usr/share/backgrounds/archlinux/arch-wallpaper.jpg &
-feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
+feh --bg-fill /usr/share/backgrounds/arco/arco-wallpaper.jpg &
 #wallpaper for other Arch based systems
 #feh --bg-fill /usr/share/archlinux-tweak-tool/data/wallpaper/wallpaper.png &
 #feh --randomize --bg-fill ~/Dropbox/Apps/Desktoppr/*
